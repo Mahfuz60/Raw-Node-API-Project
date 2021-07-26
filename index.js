6683e53ac50fd4d8b1a8718aae67203bb6f14dd0
Mahfuz <Mahfuz60>
2021-07-27 00:06:34 +0600

    code

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+Title:uptime Monitoring application
+Description:A RESTFUL API to monitor up or down time User Using Link
+Date:26/7/21
+Author:Mahfuz Alam
+*/
+// dependencies
+const http = require('http');
+
+// app object= module scaffolding
+const app = {};
+
+// configuration
+app.config = {};
